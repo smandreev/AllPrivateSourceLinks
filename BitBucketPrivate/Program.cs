2a3f38e6f585ffd143d51d135c68cf7e08ef4b94
Sergey.Andreev <sergey.andreev@jetbrains.com>
2020-10-07 18:11:37 +0300

    Initial solution

---
+++ BitBucketPrivate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BitBucketPrivate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BBSLProgam
+        }
+    }
+}
